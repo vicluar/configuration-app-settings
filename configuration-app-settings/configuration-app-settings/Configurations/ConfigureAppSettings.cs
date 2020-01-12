1c57cfb3276d08004840b9b86320172ad95b50ee
Vicluar Daniel Merchan Briceno <24820@norwegian.com>
2020-01-12 14:00:19 +0100

    Adding an extension class to load app settings values.

---
+++ configuration-app-settings/configuration-app-settings/Configurations/ConfigureAppSettings.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace configuration_app_settings.Configurations
+{
+    public static class ConfigureAppSettings
+    {
+        public static IServiceCollection AddAppSettings(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.Configure<AppSettings>(configuration);
+            return services;
+        }
+    }
+}
