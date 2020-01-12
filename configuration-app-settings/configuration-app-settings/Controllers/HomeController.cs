cdd74dd3ab6f407e6316abb04daf47acb2fb25f1
Vicluar Daniel Merchan Briceno <24820@norwegian.com>
2020-01-12 14:38:22 +0100

    Injection app settings values into home controller. Also, using those values in index view.

--- configuration-app-settings/configuration-app-settings/Controllers/HomeController.cs
+++ configuration-app-settings/configuration-app-settings/Controllers/HomeController.cs
@@ -6,20 +6,32 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using configuration_app_settings.Models;
+using configuration_app_settings.Configurations;
+using Microsoft.Extensions.Options;
 
 namespace configuration_app_settings.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly AppSettings _appSettings;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IOptionsSnapshot<AppSettings> appSettings)
         {
             _logger = logger;
+            _appSettings = appSettings.Value;
         }
 
         public IActionResult Index()
         {
+            ViewBag.HeadConfigValue1 = _appSettings.MyHeadConfiguration.HeadConfigValue1;
+            ViewBag.HeadConfigValue2 = _appSettings.MyHeadConfiguration.HeadConfigValue2;
+            
+            ViewBag.SecondLevelConfigValue1 = _appSettings.MyHeadConfiguration
+                .MySecondLevelConfig.SecondLevelConfigValue1;
+            ViewBag.SecondLevelConfigValue2 = _appSettings.MyHeadConfiguration
+                .MySecondLevelConfig.SecondLevelConfigValue2;
+
             return View();
         }
 
